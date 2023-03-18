5c91bcd2025a4e9aa8f7edd1b7d4665eff899cc9
Chatis Santos <chatis@afrointelligence.com>
2023-03-18 15:38:47 -0500

    update

--- ossDemo.c
+++ ossDemo.c
@@ -29,6 +29,7 @@ int main() {
     int i = 0;
     char *p;
     while( token != NULL ) {
+        printf("token: %s", token);
         arguments[i] = strtol(token, &p, 10);
         token = strtok(NULL, " ");
         i++;
